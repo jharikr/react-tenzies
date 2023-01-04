5a819be4c7e6f8662af391d7f98f34c0868f2839
jharikrichardson <jharik.rdson@gmail.com>
2023-01-04 16:18:09 -0500

    Add held state and toggle functionality to die

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,47 @@
 import { useState } from 'react'
 import Die from './components/Die'
+import { nanoid } from "nanoid"
 import './App.css'
 
 function App() {
-  const [dice, setDice] = useState(generateDice())
+  const [dice, setDice] = useState(generateDice(10))
 
-  function generateDice() {
-    return [...Array(10)].map(() => Math.ceil(Math.random() * 6))
+  function randomNum() {
+    return Math.ceil(Math.random() * 6)
   }
 
-  const diceElements = dice.map((dieVaL, index) =>
-    <Die key={index} value={dieVaL} />
-  )
+  function generateDice(num) {
+    return [...Array(num)].map(() => ({
+      value: randomNum(),
+      isHeld: false,
+      id: nanoid()
+    }))
+  }
+
+  function hold(id) {
+    setDice(oldDice =>
+      oldDice.map(die => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die
+      })
+    )
+  }
 
   function rollDice() {
-    setDice(generateDice())
+    setDice(oldDice =>
+      oldDice.map(die => {
+        return !die.isHeld ? { ...die, value: randomNum() } : die
+      })
+    )
   }
+
+  const diceElements = dice.map((die, index) =>
+    <Die
+      key={index}
+      value={die.value}
+      held={die.isHeld}
+      toggle={() => hold(die.id)}
+    />
+  )
 
   return (
     <main>
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,6 +1,13 @@
-function Die({ value }) {
+function Die({ value, held, toggle }) {
+    const styles = {
+        backgroundColor: held ? "#59E391" : "white"
+    }
+
     return (
-        <div className="die">
+        <div className="die"
+            style={styles}
+            onClick={toggle}
+        >
             <h2 className="die-num main-text">{value}</h2>
         </div>
     )
