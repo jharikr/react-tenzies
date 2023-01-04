7d29c492d5370f7ef77e8a50c8524985a6fe5741
jharikrichardson <jharik.rdson@gmail.com>
2023-01-04 14:59:53 -0500

    Generated rand dice vals and button to regen vals

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
+import { useState } from 'react'
 import Die from './components/Die'
 import './App.css'
 
 function App() {
+  const [dice, setDice] = useState(generateDice())
+
+  function generateDice() {
+    return [...Array(10)].map(() => Math.ceil(Math.random() * 6))
+  }
+
+  const diceElements = dice.map((dieVaL, index) =>
+    <Die key={index} value={dieVaL} />
+  )
+
+  function rollDice() {
+    setDice(generateDice())
+  }
+
   return (
     <main>
       <div className='dice'>
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
+        {diceElements}
       </div>
+      <button className='roll-dice' onClick={rollDice}>Roll</button>
     </main>
   )
 }
